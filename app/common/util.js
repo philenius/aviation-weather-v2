0e217417505b43da8207c050bda4278fd31f92c2
philenius <philippus-perez@web.de>
2018-04-18 23:44:35 +0200

    fix method call

--- app/common/util.js
+++ app/common/util.js
@@ -2,10 +2,10 @@
 
 module.exports = {
     'buildICAO': function (letter1, letter2, letter3, letter4) {
-        return getFirstLetter(letter1.toUpperCase()) +
-            getFirstLetter(letter2.toUpperCase()) +
-            getFirstLetter(letter3.toUpperCase()) +
-            getFirstLetter(letter4.toUpperCase());
+        return this.getFirstLetter(letter1.toUpperCase()) +
+            this.getFirstLetter(letter2.toUpperCase()) +
+            this.getFirstLetter(letter3.toUpperCase()) +
+            this.getFirstLetter(letter4.toUpperCase());
     },
     'getFirstLetter': function (natoLiteral) {
         return natoLiteral.charAt(0);
